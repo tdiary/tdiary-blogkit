9c40a0c66f817b221f36036e13511e25fc8ca123
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-11-20 07:26:28 +0000

    * add arcgive_dropdown plugin.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/blogkit@889 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/archive.rb
+++ plugin/archive.rb
@@ -1,16 +1,32 @@
-# archive.rb $Revision: 1.1.1.1 $
+# archive.rb $Revision: 1.2 $
 #
 # archive: 過去記事のアーカイブ一覧を表示する
 #   パラメタ: なし
 #
-def archive
-	count = 1
-	result = %Q[<ul>\n]
+def archive_make_list
+	list = []
 	@years.keys.sort.reverse_each do |year|
 		@years[year.to_s].sort.reverse_each do |month|
-			result << %Q[<li><a href="#{@index}#{anchor "#{year}#{month}"}">#{'%05d' % count}</a></li>\n]
-			count += 1
+			list << "#{year}#{month}"
 		end
 	end
+	list
+end
+
+def archive
+	result = %Q[<ul>\n]
+	archive_make_list.each_with_index do |month, count|
+		result << %Q[<li><a href="#{@index}#{anchor "#{month}"}">#{'%05d' % (count+1)}</a></li>\n]
+	end
 	result << %Q[</ul>\n]
+end
+
+def archive_dropdown( label = 'Go' )
+	result = %Q[<form method="get" action="#{@index}">\n]
+	result << %Q[<select name="date">\n]
+	archive_make_list.each_with_index do |month, count|
+		result << %Q[<option value="#{month}">#{'%05d' % (count+1)}</option>\n]
+	end
+	result << %Q[</select>\n<input type="submit" value="#{label}">\n]
+	result << %Q[</form>\n]
 end
