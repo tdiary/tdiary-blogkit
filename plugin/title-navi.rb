92042e838eabbdfdc0d47dc0c95ad6b2c4674029
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-11-24 14:56:02 +0000

    * title_navi plugin: title_navi.max option. 	* add README.en.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/blogkit@908 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/title-navi.rb
+++ plugin/title-navi.rb
@@ -1,6 +1,8 @@
-# title-navi.rb: navigation label with title of the article. $Revision: 1.2 $
+# title-navi.rb: navigation label with title of the article. $Revision: 1.3 $
 #
 # This plugin run only copy to plugin directory.
+# You can customize in tdiary.conf:
+#   @options['title_navi.max']: max length of navigation buttons (default: 30 )
 #
 # Copyright (c) 2002 TADA Tadashi <sho@spc.gr.jp>
 # Distributed under the GPL
@@ -8,7 +10,8 @@
 def navi_prev_diary( date )
 	diary = @diaries[date.strftime( '%Y%m%d' )]
 	if diary and diary.title.length > 0 then
-		diary.title
+		len = @options['title_navi.max'] || 30
+		diary.title.shorten( len.to_i )
 	else
 		"Prev"
 	end
@@ -17,7 +20,8 @@ end
 def navi_next_diary( date )
 	diary = @diaries[date.strftime( '%Y%m%d' )]
 	if diary and diary.title.length > 0 then
-		diary.title
+		len = @options['title_navi.max'] || 30
+		diary.title.shorten( len.to_i )
 	else
 		"Next"
 	end
