f9a692f2e473ef5d979cb7abd05168fa74dd7e78
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-04-25 09:14:13 +0000

    * description into RDF.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/blogkit@1340 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/whatsnew-list.rb
+++ plugin/whatsnew-list.rb
@@ -1,4 +1,4 @@
-# whatsnew-list.rb: what's new list plugin $Revision: 1.9 $
+# whatsnew-list.rb: what's new list plugin $Revision: 1.10 $
 #
 # whatsnew_list: show what's new list
 #   parameter (default):
@@ -10,6 +10,8 @@
 #         if this is true, apply plugin to result. (false)
 #      @options['whatsnew_list.rdf']
 #         RDF file path. (nil)
+#      @options['whatsnew_list.rdf.description']
+#         description of the site in RDF. (@html_title)
 #
 #   notice:
 #     This plugin dose NOT run on secure mode.
@@ -58,12 +60,14 @@ def whatsnew_list_rdf( items )
 	path.sub!( /#{@conf.update}/, @conf.index )
 	path.sub!( /\/\.\//, '/' )
 
+	desc = @options['whatsnew_list.rdf.description'] || @conf.html_title
+
 	xml = %Q[<?xml version="1.0" encoding="#{charset}"?>
 	<rdf:RDF xmlns="http://purl.org/rss/1.0/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xml:lang="#{@conf.lang ? @conf.lang : 'ja'}">
 	<channel>
 	<title>#{@conf.html_title}</title>
 	<link>#{path}</link>
-	<description>#{@conf.html_title}</description>
+	<description>#{desc}</description>
 	]
 
 	xml << %Q[<items><rdf:Seq>\n]
