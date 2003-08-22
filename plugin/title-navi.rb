5d469a8006e40c29dd950881410a489d47dbbe80
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-08-22 07:53:22 +0000

    * title-navi.rb: show title in the prev/next month. 	* tdiary.conf.sample*: remove comment mail setting.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/blogkit@1495 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/title-navi.rb
+++ plugin/title-navi.rb
@@ -1,4 +1,4 @@
-# title-navi.rb: navigation label with title of the article. $Revision: 1.3 $
+# title-navi.rb: navigation label with title of the article. $Revision: 1.4 $
 #
 # This plugin run only copy to plugin directory.
 # You can customize in tdiary.conf:
@@ -7,6 +7,50 @@
 # Copyright (c) 2002 TADA Tadashi <sho@spc.gr.jp>
 # Distributed under the GPL
 #
+
+if @mode == 'day'
+	eval( <<-MODIFY_CLASS, TOPLEVEL_BINDING )
+	module TDiary
+		class TDiaryMonth
+			attr_reader :diaries
+		end
+	end
+	MODIFY_CLASS
+
+	month = @date.strftime( '%Y%m' )
+	years = @years.collect{|y,ms| ms.collect{|m| "#{y}#{m}"}}.flatten.sort
+	cgi = CGI::new
+	def cgi.referer; nil; end
+
+	# search pre
+	pre = 0
+	(@date.day - 1).downto( 1 ) do |day|
+		diary = @diaries[month + ('%02d' % day)]
+		pre = day if diary and diary.visible?
+	end
+	if pre == 0 then
+		if years.index( month ) - 1 >= 0 then
+			cgi.params['date'] = [years[years.index( month ) - 1]]
+			m = TDiaryMonth::new( cgi, '', @conf )
+			@diaries.update( m.diaries )
+		end
+	end
+
+	# search nex
+	nex = 0
+	(@date.day + 1).upto( 31 ) do |day|
+		diary = @diaries[month + ('%02d' % day)]
+		nex = day if diary and diary.visible?
+	end
+	if nex == 0 then
+		if years.index( month ) + 1 < years.size then
+			cgi.params['date'] = [years[years.index( month ) + 1]]
+			m = TDiaryMonth::new( cgi, '', @conf )
+			@diaries.update( m.diaries )
+		end
+	end
+end
+
 def navi_prev_diary( date )
 	diary = @diaries[date.strftime( '%Y%m%d' )]
 	if diary and diary.title.length > 0 then
