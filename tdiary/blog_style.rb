015869094336433efbfc9d9b14f317ec973a9e67
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-09-10 07:10:20 +0000

    * blog_style.rb: support to getnerate new date in Append mode.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/blogkit@1525 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary/blog_style.rb
+++ tdiary/blog_style.rb
@@ -1,5 +1,5 @@
 #
-# blog_style.rb: tDiary blog kit's style $Revision: 1.3 $
+# blog_style.rb: tDiary blog kit's style $Revision: 1.4 $
 #
 # if you want to use this style, add @style into tdiary.conf below:
 #
@@ -53,27 +53,52 @@ module TDiary
 		end
 	end
 
+	class TDiaryBase
+	protected
+		def blog_newdate( date )
+			calendar
+			year = @years.keys.sort[-1]
+			if year then
+				month = @years[year].sort[-1]
+				if month then
+					@io.transaction( Time::local( year.to_i, month.to_i ) ) do |diaries|
+						recent = diaries.keys.sort[-1]
+						if date.strftime( '%Y%m%d' ) <= recent then
+							yield( recent )
+						end
+						DIRTY_NONE
+					end
+				end
+			end
+		end
+	end
+
 	class TDiaryForm
 		alias eval_rhtml_blogkit eval_rhtml
 		def eval_rhtml( prefix = '' )
 			if /^blog/i =~ @conf.style then
-				calendar
-				year = @years.keys.sort[-1]
-				if year then
-					month = @years[year].sort[-1]
-					if month then
-						@io.transaction( Time::local( year.to_i, month.to_i ) ) do |diaries|
-							recent = diaries.keys.sort[-1]
-							if @date.strftime( '%Y%m%d' ) <= recent then
-								@date = Time::local( *recent.scan( /(\d{4})(\d\d)(\d\d)/ )[0] ) + 24*60*60
-								@diary = @io.diary_factory( @date, '', '' )
-							end
-							DIRTY_NONE
-						end
-					end
+				blog_newdate( @date ) do |recent|
+					@date = Time::local( *recent.scan( /(\d{4})(\d\d)(\d\d)/ )[0] ) + 24*60*60
+					@diary = @io.diary_factory( @date, '', '' )
 				end
 			end
 			eval_rhtml_blogkit( prefix )
+		end
+	end
+
+	class TDiaryAppend
+	protected
+		alias newdate_blogkit newdate
+		def newdate
+			date = nil
+			if /^blog/i =~ @conf.style then
+				blog_newdate( newdate_blogkit ) do |recent|
+					date = Time::local( *recent.scan( /(\d{4})(\d\d)(\d\d)/ )[0] ) + 24*60*60
+				end
+			else
+				date = newdate_blogkit
+			end
+			date
 		end
 	end
 end
