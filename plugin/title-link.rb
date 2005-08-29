94b5ddf9b4d2af6ee019cf5ff441fb717cf89968
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-08-29 12:47:28 +0000

    * plugin/title-link.rb: disable link in day mode. 	* README*: add about title-link.rb plugin.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/blogkit@2347 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/title-link.rb
+++ plugin/title-link.rb
@@ -1,5 +1,5 @@
 #
-# title-link.rb: make permalink include all of title. $Revision: 1.1 $
+# title-link.rb: make permalink include all of title. $Revision: 1.2 $
 #
 # Copyright (C) 2005 TADA Tadashi <sho@spc.gr.jp>
 # You can redistribute it and/or modify it under GPL2.
@@ -20,11 +20,13 @@ def title_of_day( date, title )
 	r = <<-HTML
 	<span class="title">
 	#{cats}
-	<a href="#{@index}#{anchor( date.strftime( '%Y%m%d' ) )}">
-	#{stripped}
-	</a>
-	</span>
 	HTML
+	if @mode == 'day' then
+		r << stripped
+	else
+		r << %Q[<a href="#{@index}#{anchor( date.strftime( '%Y%m%d' ) )}">#{stripped}</a>]
+	end
+	r << "</span>"
 	return r.gsub( /^\t+/, '' ).chomp
 end
 
