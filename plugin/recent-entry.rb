350573238fcbe03cbbb30924f0bc3ed119a63e72
TADA Tadashi <t@tdtds.jp>
2017-09-08 16:02:49 +0900

    update to modern style and fix some information

--- plugin/recent-entry.rb
+++ plugin/recent-entry.rb
@@ -1,33 +1,31 @@
-# recent_entry.rb $Revision: 1.13 $
-#
 # recent_entry: modified 'title_list' for Blogkit.
 #   parameter(default):
 #     max:       maximum list items (5)
 #     limit:     max lengh of each items (20)
 #
-# Copyright (c) 2002 TADA Tadashi <sho@spc.gr.jp>
+# Copyright (c) 2002 TADA Tadashi <t@tdtds.jp>
 # Copyright (c) 2001,2002 Junichiro KITA <kita@kitaj.no-ip.com>
-# You can redistribute it and/or modify it under GPL2.
+# You can redistribute it and/or modify it under GPL.
 #
 
-def recent_entry( max = 5, limit = 20 )
+def recent_entry(max = 5, limit = 20)
 	max = max.to_i
 	limit = limit.to_i
 
 	result = "<ul>\n"
-	catch( :exit ) {
+	catch(:exit) {
 		@years.keys.sort.reverse_each do |year|
 			@years[year].sort.reverse_each do |month|
 				m = DiaryContainer::find_by_month(@conf, "#{year}#{month}")
 				m.diaries.keys.sort.reverse_each do |date|
 					next unless m.diaries[date].visible?
-					title = if m.diaries[date].respond_to?( :stripped_title ) then
-						title = m.diaries[date].stripped_title.gsub( /<[^>]*>/, '' )
+					title = if m.diaries[date].respond_to?(:stripped_title) then
+						title = m.diaries[date].stripped_title.gsub(/<[^>]*>/, '')
 					else
-						title = m.diaries[date].title.gsub( /<[^>]*>/, '' )
+						title = m.diaries[date].title.gsub(/<[^>]*>/, '')
 					end
 					title = 'no title' if title.empty?
-					result << %Q|<li><a href="#{h @index}#{anchor date}">#{@conf.shorten( title, limit )}</a></li>\n|
+					result << %Q|<li><a href="#{h @index}#{anchor date}">#{@conf.shorten(title, limit)}</a></li>\n|
 					max -= 1
 					throw :exit if max == 0
 				end
@@ -35,7 +33,7 @@ def recent_entry( max = 5, limit = 20 )
 		end
 	}
 	result << "</ul>\n"
-	apply_plugin( result )
+	apply_plugin(result)
 end
 
 # Local Variables:
